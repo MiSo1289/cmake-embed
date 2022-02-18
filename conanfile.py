7f6a6af8c99a49dc3cc901c64d716eabcfdae1fc
Michal Jankovič <michal.jankovic@sanezoo.com>
2022-02-18 10:36:12 +0100

    Use builddirs property in conanfile instead of a cmake config file

--- conanfile.py
+++ conanfile.py
@@ -3,13 +3,15 @@ from conans import ConanFile
 
 class CMakeEmbed(ConanFile):
     name = "cmake_embed"
-    version = "0.1.0"
+    version = "0.2.0"
     revision_mode = "scm"
-    exports_sources = "cmake/*", "EmbedResourcesConfig.cmake"
+    exports_sources = "cmake/*"
+
+    def package(self):
+        self.copy("*.cmake", dst="cmake", src="cmake")
 
     def package_id(self):
         self.info.header_only()
 
-    def package(self):
-        self.copy("*.cmake", dst="cmake", src="cmake")
-        self.copy("EmbedResourcesConfig.cmake", dst=".", src=".")
+    def package_info(self):
+        self.cpp_info.builddirs = ["cmake"]
